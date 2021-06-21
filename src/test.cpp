659ec0a5b48b297035726e893bdf1b7fa10ccba3
shrpbv <asheripbaev@miem.hse.ru>
2021-06-21 07:53:29 +0300

    test

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,4 @@
 #include "doctest.h"
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <string>
-#include <ctime>
 #include "lib.hpp"
 
 using namespace std;
@@ -12,3 +7,34 @@ TEST_CASE("simpleTest"){
     int a = 1;
     REQUIRE(a == 1);
 }
+
+template<class T>
+class Point
+{
+public:
+    T x;
+    T y;
+
+    Point(T x = 0, T y = 0)
+    {
+        this->x = x;
+        this->y = y;
+    }
+    ~Point() {};
+
+    bool operator < (Point p)
+    {
+        return (this->x < p.x) and (this->y < p.y);
+    }
+
+    bool operator > (Point p)
+    {
+        return (this->x > p.x) and (this->y > p.y);
+    }
+
+    bool operator == (Point p)
+    {
+        return (this->x == p.x) and (this->y == p.y);
+    }
+};
+
